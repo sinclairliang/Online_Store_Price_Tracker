f9fa22bf5013857596f650f057b4ef180ca43c9c
Sinclair Liang <sinclairliang@gmail.com>
2018-09-14 09:39:19 +0800

    created methods to encrypt passwords and verification

---
+++ src/commom/utils.py
@@ -0,0 +1,26 @@
+from passlib.hash import pbkdf2_sha512
+
+
+class Utils(object):
+
+    @staticmethod
+    def hash_password(password):
+        """
+        Hashes a password using pbkdf2_sha512, double encryption
+        :param password: the sha512 password from Login/Register
+        :return: a sha512->pbkdf2_sha512 encrypted password
+        """
+        return pbkdf2_sha512.encrypt(password)
+
+
+    @staticmethod
+    def check_hashed_password(password, hashed_password):
+        """
+        Checks the password user sent matches with the password stored in database
+        The database password is encrypted more than the one user sent
+        :param password: sha512 hashed
+        :param hashed_password: pbkdf2_sha512 encrypted password
+        :return: Boolean value
+        """
+        return pbkdf2_sha512.verify(password, hashed_password)
+
--- src/models/users/user.py
+++ src/models/users/user.py
@@ -1,7 +1,32 @@
+import uuid
+
+from src.commom.database import Database
+from src.commom.utils import Utils
+
 class User(object):
-    def __init__(self, email, password):
+    def __init__(self, email, password, _id=None):
         self.email = email
         self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
         return"---User {}---".format(self.email)
+
+
+    @staticmethod
+    def login_valid(email, password):
+        """
+        This method verifies email/password combo is valid or not
+        Checks if the email exists, and the password is correct to it
+        :param email: user's email
+        :param password: a sha512 hashed password
+        :return: Boolean value
+        """
+        user_date = Database.find_one("users", {"email": email})
+        if user_date is None:
+            # user does not exist
+            pass
+        if not Utils.check_hashed_password(password, user_data['password']):
+            # the password is not valid
+            pass
+        return True
