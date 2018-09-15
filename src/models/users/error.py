8c40d67747aa2d600c3200800c82c40bed559578
Sinclair Liang <sinclairliang@gmail.com>
2018-09-15 22:53:39 +0800

    unified Exceptions in various files

--- src/models/users/error.py
+++ src/models/users/error.py
@@ -1,21 +1,11 @@
-# class UserError(Exception):
-#     def __init__(self, message):
-#         self.message = message
-#
-#
-# class UserNotExistException(UserError):
-#         pass
-#
-#
-# class IncorrectPasswordException(UserError):
-#         pass
-
-
-class UserNotExistException(Exception):
+class UserError(Exception):
     def __init__(self, message):
         self.message = message
 
 
-class IncorrectPasswordException(Exception):
-    def __init__(self, message):
-        self.message = message
+class UserNotExistException(UserError):
+    pass
+
+
+class IncorrectPasswordException(UserError):
+    pass
