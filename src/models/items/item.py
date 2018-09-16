e3460237aa83b4745ce792bdc98b3f0319a1969d
Sinclair Liang <sinclairliang@gmail.com>
2018-09-16 18:00:30 +0800

    spent one hour trying to debug...

--- src/models/items/item.py
+++ src/models/items/item.py
@@ -1,12 +1,14 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
 import re
+
+from src.commom.database import Database
+import src.models.items.constants as ItemConstants
 
 
 class Item(object):
     def __init__(self, name, url, store):
         self.name = name
-
         self.url = url
         self.store = store
         tag_name = store.get_tag_name()
@@ -23,10 +25,25 @@ class Item(object):
     def load_item_price(self, tag_name, query):
         request = requests.get(self.url)
         content = request.content
-        soup = BeautifulSoup(content, "html.parser")
+        soup = bs4.BeautifulSoup(content, "html.parser")
         element = soup.find(tag_name, query)
         string_price = element.text.strip()
-
         pattern = re.compile("(\d+.\d+)") # 74.37
         match = pattern.search(string_price)
+        print(match)
         return match.group()
+
+    def save_to_db(self):
+        Database.insert(ItemConstants.COLLECTION, self.json)
+
+    def json(self):
+        return {
+            "name": self.name,
+            "url": self.url,
+            "price": self.price
+        }
+
+    @classmethod
+    def retrieve_item(cls, url):
+        item_data = Database.find_one(ItemConstants.COLLECTION, url)
+        return cls(**item_data)
--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -1,13 +1,15 @@
 class Store(object):
-    def __init__(self, name, url_prefix, ):
+    def __init__(self, name, url_prefix, tag_name, query):
         self.name = name
         self.url_prefix = url_prefix
+        self.tag_name = tag_name
+        self.query = query
 
     def __repr__(self):
         return "---Store {}---".format(self.name)
 
     def get_tag_name(self):
-        pass
+        return self.tag_name
 
     def get_query_name(self):
-        pass
+        return self.query
