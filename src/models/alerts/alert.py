f8330772ae13b79198c7f78b15f38e1ce55ce5cc
Sinclair Liang <sinclairliang@gmail.com>
2018-09-18 13:07:24 +0800

    finished constructing Alert object

--- src/models/alerts/alert.py
+++ src/models/alerts/alert.py
@@ -5,20 +5,20 @@ import src.models.alerts.constants as AlertConstants
 import src.models.alerts.login as LoginInfo
 
 import datetime
-
 from src.commom.database import Database
+from src.models.items.item import Item
 
 
 class Alert(object):
-    def __init__(self, user, price_limit, item, last_check=None, _id=None):
-        self.user = user
+    def __init__(self, user_email, price_limit, item_id, last_check=None, _id=None):
+        self.user_email = user_email.email
         self.price_limit = price_limit
-        self.item = item
+        self.item = Item.get_by_id(item_id)
         self.last_check = datetime.datetime.utcnow() if last_check is None else last_check
         self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
-        return "---Alert for {} on item {} with the price limit {}---".format(self.user.email,
+        return "---Alert for {} on item {} with the price limit {}---".format(self.user_email,
                                                                               self.item.name,
                                                                               self.item.price_limit)
 
@@ -26,10 +26,8 @@ class Alert(object):
         msg = MIMEText("We found you a deal for you. Click on link: {}".format(self.item.url))
         msg['Subject'] = 'Price Reached for item{}!'.format(self.item.name)
         msg['From'] = LoginInfo.LOGIN
-        msg['To'] = self.user.email
-
+        msg['To'] = self.user_email
         s = smtplib.SMTP('smtp.mailgun.org', 587)
-
         s.login(LoginInfo.LOGIN, LoginInfo.PASSWORD)
         if len(s.sendmail(msg['From'], msg['To'], msg.as_string())) == 0:
             return True
@@ -40,4 +38,17 @@ class Alert(object):
     def update(cls, time_since_update=10):
         last_updated_limit = datetime.datetime.utcnow() - datetime.timedelta(minutes=time_since_update)
         return [cls(**element) for element in Database.find(AlertConstants.COLLECTION,
-                                                            {"last_checked": {"$gte": last_updated_limit}})]
+                                                            {"last_checked":
+                                                            {"$gte": last_updated_limit}})]
+
+    def save_to_mongo(self):
+        Database.insert(AlertConstants.COLLECTION, self.json())
+
+    def json(self):
+        return{
+            "price_limit": self.price_limit,
+            "last_check": self.last_check,
+            "_id": self._id,
+            "user_email": self.user_email,
+            "item_id": self.item._id
+        }
--- src/models/items/item.py
+++ src/models/items/item.py
@@ -5,16 +5,17 @@ import uuid
 
 from src.commom.database import Database
 import src.models.items.constants as ItemConstants
+from src.models.stores.store import Store
 
 
 class Item(object):
-    def __init__(self, name, url, store, _id=None):
+    def __init__(self, name, url, _id=None):
         self.name = name
         self.url = url
-        self.store = store
-        tag_name = store.get_tag_name()
-        query = store.get_query_name()
-        self.price = self.load_item_price(tag_name, query)
+        store = Store.get_by_url(url)
+        self.tag_name = store.get_tag_name()
+        self.query = store.get_query_name()
+        self.price = None
         self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
@@ -24,15 +25,16 @@ class Item(object):
                                                                              self.url)
         return "---Item {} with the URL {}---".format(self.name, self.url)
 
-    def load_item_price(self, tag_name, query):
+    def load_item_price(self):
         request = requests.get(self.url)
         content = request.content
         soup = bs4.BeautifulSoup(content, "html.parser")
-        element = soup.find(tag_name, query)
+        element = soup.find(self.tag_name, self.query)
         string_price = element.text.strip()
-        pattern = re.compile("(\d+.\d+)") # 74.37
+        pattern = re.compile("(\d+.\d+)")
         match = pattern.search(string_price)
-        return match.group()
+        self.price = match.group()
+        return self.price
 
     def save_to_mongo(self):
         Database.insert(ItemConstants.COLLECTION, self.json())
@@ -49,3 +51,7 @@ class Item(object):
     def retrieve_item(cls, url):
         item_data = Database.find_one(ItemConstants.COLLECTION, url)
         return cls(**item_data)
+
+    @classmethod
+    def get_by_id(cls, item_id):
+        return cls(**Database.find_one(ItemConstants.COLLECTION, {"_id": item_id}))
