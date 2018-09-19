c1a44752a9bfabb734e200c41668f5953144cefd
Sinclair Liang <sinclairliang@gmail.com>
2018-09-19 16:26:27 +0800

    working on pages, CSS and HTML contents

--- src/app.py
+++ src/app.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from src.commom.database import Database
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('src.config')
 app.secret_key = "1479"
 
 
 @app.before_first_request
 def init_db():
     Database.initialize()
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 
 from src.models.users.views import user_blueprint
