0fcb4d3c24f2ef4d693179404ce6d7c9bad77010
Sinclair Liang <sinclairliang@gmail.com>
2018-09-20 18:40:10 +0800

    changed a few names

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ def init_db():
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.jinja2')
 
 
 from src.models.users.views import user_blueprint
