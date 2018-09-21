450e6974582fc0dde6e928ea61598231bc67af59
Sinclair Liang <sinclairliang@gmail.com>
2018-09-21 10:09:40 +0800

    refactor names

--- src/models/users/views.py
+++ src/models/users/views.py
@@ -18,7 +18,7 @@ def login_user():
                 return redirect(url_for(".user_alerts"))
         except UserErrors.UserError as e:
             return e.message
-    return render_template("users/login.html") # wish to return a pop-up window
+    return render_template("users/login.jinja2") # wish to return a pop-up window
 
 
 @user_blueprint.route('/register', methods=['GET', 'POST'])
@@ -32,7 +32,7 @@ def register_user():
                 return redirect(url_for(".user_alerts"))
         except UserErrors.UserError as e:
                 return e.message
-    return render_template("users/register.html") # wish to return a pop-up window
+    return render_template("users/register.jinja2") # wish to return a pop-up window
 
 
 @user_blueprint.route('/alerts')
