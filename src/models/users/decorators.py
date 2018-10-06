d5a981bb28d65187b3ee4398e8d36cfbdba6d0d2
Sinclair Liang <sinclairliang@gmail.com>
2018-10-06 01:58:02 -0700

    messing up with decorator

--- src/models/users/decorators.py
+++ src/models/users/decorators.py
@@ -1,8 +1,1 @@
-def requires_login(func):
-    func()
-
-
-@requires_login
-def my_function():
-    print("Hello World")
-    return "Hi"
+from functools import wrapsfrom flask import session, redirect, url_for, requestdef requires_login(func):    @wraps(func)    def decorated_function(*args, **kwargs):        if 'email' not in session.keys() or session['email'] is None:            return redirect(url_for('user.login_user', next=request.path))        return func(*args, **kwargs)    return decorated_function
