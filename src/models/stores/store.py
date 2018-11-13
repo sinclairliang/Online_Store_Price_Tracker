852f82a347b6d2eb48c4ca9259e61cd8c1b26890
Sinclair Liang <sinclairliang@gmail.com>
2018-11-12 21:44:19 -0800

    finished edit store page

--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -39,7 +39,7 @@ class Store(object):
         return cls(**Database.find_one(StoreConstants.COLLECTION, {"_id": id}))
 
     def save_to_mongo(self):
-        Database.insert(StoreConstants.COLLECTION, self.json())
+        Database.update(StoreConstants.COLLECTION, {'_id': self._id}, self.json())
 
     @classmethod
     def get_by_name(cls, store_name):
--- src/models/stores/views.py
+++ src/models/stores/views.py
@@ -33,9 +33,23 @@ def create_store():
 
 @store_blueprint.route('/edit/<string:store_id>', methods=['GET', 'POST'])
 def edit_store(store_id):
+    store = Store.get_by_id(store_id)
+
     if request.method == 'POST':
-        pass
-    return "Edit store Page"
+        name = request.form['name']
+        url_prefix = request.form['url_prefix']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form["query"])
+
+
+        store.name = name
+        store.url_prefix = url_prefix
+        store.tag_name = tag_name
+        store.query = query
+
+        store.save_to_mongo()
+        return redirect(url_for('.index'))
+    return render_template('stores/edit_store.jinja2', store=store)
 
 
 @store_blueprint.route('/delete/<string:store_id>')
